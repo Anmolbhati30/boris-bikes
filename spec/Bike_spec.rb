d882367c629d6b3f2f7e16a836610a5c490088e8
Anmol <bhatianmol566@yahoo.co.uk>
2021-09-20 16:54:14 +0100

    Add Bike files

---
+++ spec/Bike_spec.rb
@@ -0,0 +1,11 @@
+require 'Bike'
+
+describe Bike do
+  it "Create a new Bike object" do
+    bike = Bike.new
+  end
+
+  it "Respond to the method 'working?'" do 
+    expect(Bike.new).to respond_to(:working?)
+  end
+end
