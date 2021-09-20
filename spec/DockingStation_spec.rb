07af58682a8cb7cb7b7feb1e25fdc9ea53d8f5ef
Anmol <bhatianmol566@yahoo.co.uk>
2021-09-20 16:47:55 +0100

    Add release_bike method to DockingStation

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -3,4 +3,8 @@ describe DockingStation do
   it "Create a new DockingStation object" do
     dock = DockingStation.new
   end
+
+  it "Respond to release_bike" do
+    expect(DockingStation.new).to respond_to(:release_bike)
+  end
 end
