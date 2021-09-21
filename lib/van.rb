c0dbd92b07f71ddcaeae120d06941785ba98e906
Anmol <bhatianmol566@yahoo.co.uk>
2021-09-21 16:58:39 +0100

    Add initial tests for Van

--- lib/van.rb
+++ lib/van.rb
@@ -1,3 +1,17 @@
 class Van
+  attr_reader :capacity
 
+  @DEFAULT_CAPACITY = 5
+
+  class << self
+    attr_reader :DEFAULT_CAPACITY
+  end
+  
+  def initialize(capacity = Van.DEFAULT_CAPACITY)
+    @capacity = capacity
+  end
+
+  def collect_from(station)
+  
+  end
 end
--- spec/Van_spec.rb
+++ spec/Van_spec.rb
@@ -1,5 +1,24 @@
 require 'van'
 
 describe Van do
+  it "Create a new Van object" do
+    van = Van.new
+  end
+
+  it  { is_expected.to respond_to(:collect_from).with(1).argument }
+
+
+  it  "Returns available capacity" do
+    expect(subject.capacity).to be_a(Integer)
+  end
+
+  it "Can be initialised with a capacity" do
+    van = Van.new(6)
+    expect(van.capacity).to eq 6
+  end
+
+  it "initializes with a default capacity" do
+    expect(subject.capacity).to eq Van.DEFAULT_CAPACITY
+  end
 
 end
