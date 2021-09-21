84d82df8b6e5db39621f55dca312cf8615061f32
Anmol <bhatianmol566@yahoo.co.uk>
2021-09-21 16:31:24 +0100

    Isolate with doubles for DockingStation

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,5 +1,8 @@
 require "docking_station"
 describe DockingStation do 
+  let(:bike) { double :bike, :working? => true } 
+  let(:broken_bike) { double :broken_bike, :working? => false}
+
   it "Create a new DockingStation object" do
     station = DockingStation.new
   end
@@ -9,10 +12,11 @@ describe DockingStation do
   end
 
   describe '#release_bike' do
-    it 'releases a bike' do
-      bike = Bike.new
+    it 'releases a working bike' do
       subject.dock(bike)
-      expect(subject.release_bike).to eq bike
+      released_bike = expect(subject.release_bike)
+
+      expect(bike).to be_working
     end
   end
 
@@ -20,13 +24,11 @@ describe DockingStation do
  
   it "Docks bike" do
     station = DockingStation.new
-    bike = Bike.new
     expect(station.dock(bike)).to eq bike
   end
 
   it "Returns docked bikes" do
     station = DockingStation.new
-    bike = Bike.new
     station.dock(bike)
     expect(station.bikes).to eq [bike]
   end
@@ -37,26 +39,23 @@ describe DockingStation do
 
   it "Sets capacity to 6. Returns error message when bikes is full" do
     station = DockingStation.new(6)
-    6.times { station.dock(Bike.new) }
-    expect { station.dock(Bike.new) }.to raise_error 'Docking station is full!'
+    6.times { station.dock(bike) }
+    expect { station.dock(bike) }.to raise_error 'Docking station is full!'
   end
 
   it "Returns error message when bikes is full" do
     station = DockingStation.new
-    20.times { station.dock(Bike.new) }
-    expect { station.dock(Bike.new) }.to raise_error 'Docking station is full!'
+    20.times { station.dock(bike) }
+    expect { station.dock(bike) }.to raise_error 'Docking station is full!'
   end
 
   it "Doesn't release broken bikes" do
     station = DockingStation.new
-    2.times { station.dock(Bike.new) }
-    bike = Bike.new
-    bike.report_broken
-    station.dock(bike)
+    2.times { station.dock(bike) }
+    3.times { station.dock(broken_bike) }
     2.times { station.release_bike }
 
     expect { station.release_bike }.to raise_error 'No bikes available!'
   end
-  
 
 end
